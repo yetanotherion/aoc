a791d3350b2225c47909836103b7fae45c2bc685
Ion Alberdi <ion@Ions-MacBook-Pro.local>
2024-12-23 09:03:32 +0100

    Add 22/2

--- 2024/22/main.py
+++ 2024/22/main.py
@@ -31,6 +31,37 @@ def question_one(numbers):
     return sum(res)
 
 
+def question_two(numbers, rounds=2000):
+    numbers_keys = []
+    for n in numbers:
+        next_s = n
+        curr_res = []
+        before = int(str(next_s)[-1])
+        s_codes = {}
+        for _ in range(rounds):
+            next_s = next_secret_number(next_s)
+            curr_digit = int(str(next_s)[-1])
+            delta = curr_digit - before
+            before = curr_digit
+            if len(curr_res) == 4:
+                curr_res.pop(0)
+            curr_res.append(delta)
+            curr_key = tuple(curr_res)
+            if curr_key not in s_codes and len(curr_key) == 4:
+                s_codes[curr_key] = curr_digit
+        numbers_keys.append(s_codes)
+    curr_max = 0
+    all_keys = set()
+    for n in numbers_keys:
+        all_keys = all_keys.union(set(n.keys()))
+    for k in all_keys:
+        all_values = [d.get(k, 0) for d in numbers_keys]
+        curr_res = sum(all_values)
+        curr_max = max(curr_res, curr_max)
+    return curr_max
+
+
 if __name__ == "__main__":
     game_input = parse_input(read_lines(fname="input.txt"))
     print(question_one(game_input))
+    print(question_two(game_input))

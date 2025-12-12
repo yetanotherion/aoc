7e656d967bcc74bda42b3156cedfb4a54fdd47df
Ion Alberdi <ion.alberdi@joinzoe.com>
2025-12-12 15:12:06 +0100

    Simplify

--- 2025/11/main.py
+++ 2025/11/main.py
@@ -18,7 +18,6 @@ def all_paths(
     memory=None,
     start="you",
     out="out",
-    skip=(),
 ):
     if memory is None:
         memory = {}
@@ -27,7 +26,7 @@ def all_paths(
     next = graph.get(start, [])
     for n in next:
         if n not in memory:
-            memory[n] = all_paths(graph, memory=memory, start=n, out=out, skip=skip)
+            memory[n] = all_paths(graph, memory=memory, start=n, out=out)
     return sum(memory[n] for n in next)
 
 
@@ -38,12 +37,12 @@ def question_one(fname):
 
 def question_two(fname):
     graph = load_graph(fname)
-    res_svr_fft = all_paths(graph, start="svr", out="fft", skip=("dac", "out"))
-    res_fft_dac = all_paths(graph, start="fft", out="dac", skip=("svr", "out"))
-    res_dac_fft = all_paths(graph, start="dac", out="fft", skip=("svr", "out"))
-    res_svr_dac = all_paths(graph, start="svr", out="dac", skip=("fft", "out"))
-    res_fft_out = all_paths(graph, start="fft", out="out", skip=("svr", "dac"))
-    res_dac_out = all_paths(graph, start="dac", out="out", skip=("svr", "fft"))
+    res_svr_fft = all_paths(graph, start="svr", out="fft")
+    res_fft_dac = all_paths(graph, start="fft", out="dac")
+    res_dac_fft = all_paths(graph, start="dac", out="fft")
+    res_svr_dac = all_paths(graph, start="svr", out="dac")
+    res_fft_out = all_paths(graph, start="fft", out="out")
+    res_dac_out = all_paths(graph, start="dac", out="out")
     return (
         res_svr_fft * res_fft_dac * res_dac_out
         + res_svr_dac * res_dac_fft * res_fft_out
